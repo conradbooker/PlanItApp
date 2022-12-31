ebfa0fe42a1bd39587419408065e6ae88f5e4fc1
conradbooker <conrad.booker@gmail.com>
2022-12-31 13:47:33 -0500

    Initial Commit

---
+++ PlanIt/PlanItApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlanItApp.swift
+//  PlanIt
+//
+//  Created by Conrad on 12/31/22.
+//
+
+import SwiftUI
+
+@main
+struct PlanItApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
