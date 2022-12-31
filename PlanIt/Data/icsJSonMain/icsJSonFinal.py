b7f6ab81621b0c0be1a01a500d045ef5b34a9697
conradbooker <conrad.booker@gmail.com>
2022-12-31 15:55:53 -0500

    Shifted files

---
+++ PlanIt/Data/icsJSonMain/icsJSonFinal.py
@@ -0,0 +1,21 @@
+import icsJSon
+import json 
+import pyperclip
+# import requests
+
+
+def returnicsJSon(path, type = "web"):
+
+    if path == None:
+        return "\nError: path must be specified.\n"
+
+    if type == "web":
+        result = [icsJSon.fromWeb(path)]
+
+    elif type == "file":
+        result = [icsJSon.fromFile(path)]
+
+    else:
+        result = [icsJSon.fromText(path)]
+
+    return(json.dumps(result, indent=3, separators=(", ", ": ")))
